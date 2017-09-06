ea3e4fb214b24a49b4dcf4e20b9d2de1bcd93fc7
Aaron Albert <aaron8bit@gmail.com>
2017-09-05 23:23:29 -0400

    Added egress rule for all traffic

--- vpc144/ec2-bastion.tf
+++ vpc144/ec2-bastion.tf
@@ -32,5 +32,12 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   vpc_id = "${aws_vpc.vpc144.id}"
 }
